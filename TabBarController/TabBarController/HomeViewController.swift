5f288b02149f448d3eaf0a06b934093548a1d679
Uladzimir Kulakou <rombik9@ya.ru>
2022-06-22 16:51:42 +0300

    commit 3
    it s work

--- TabBarController/TabBarController/HomeViewController.swift
+++ TabBarController/TabBarController/HomeViewController.swift
@@ -11,6 +11,6 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemOrange
     }
 }
---
+++ TabBarController/TabBarController/UIColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColorExtension.swift
+//  TabBarController
+//
+//  Created by Uladzimir Kulakou on 6/22/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        #colorLiteral(red: 0.2342318874, green: 0.07228251745, blue: 0.4149718137, alpha: 1)
+    }
+    static var mainWhite: UIColor {
+        #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    static var tabBarItemLight: UIColor {
+        #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    }
+}
